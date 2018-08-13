f5634597497ad817d51a46bb6d0b8ec4eadc19cc
douglas fletcher <fletchy_d@hotmail.com>
2018-08-13 17:15:45 +0200

    DFO: update spark to java8

---
+++ src/main/java/ProjectStaticVars.java
@@ -0,0 +1,15 @@
+public class ProjectStaticVars {
+
+    public static final String projLoc = "C:/Users/dofletcher/Documents/deloitte/projects/creditscoring/";
+
+    public static String createFileLoc(String fileIn){
+        // file location
+        String outfile = new StringBuilder(projLoc).append(fileIn).toString();
+        return outfile ;
+    }
+
+    public static String getProjloc() {
+        return projLoc;
+    }
+
+}
