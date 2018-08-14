1cdf12fe08d250a46b7f159fd3564944c71cade7
douglas fletcher <-->
2018-08-14 15:43:58 +0200

    DFO: add base test files

---
+++ src/test/java/utility/InstanceSparkSessionTest.java
@@ -0,0 +1,14 @@
+package utility;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class InstanceSparkSessionTest {
+
+    @Test
+    public void getInstance_shouldSucceed(){
+        assertTrue(true);
+    }
+
+}
