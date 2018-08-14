40cd39c3f91fc8f6c64a0c16ada553f67971572f
douglas fletcher <-->
2018-08-14 15:43:58 +0200

    DFO: add base test files

---
+++ src/test/java/data/ReadDatasetTest.java
@@ -0,0 +1,19 @@
+package data;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ReadDatasetTest {
+
+    @Test
+    public void readRawData_shouldSucceed(){
+        assertTrue(true);
+    }
+
+    @Test
+    public void getPreppedData_shouldSucceed(){
+        assertTrue(true);
+    }
+
+}
