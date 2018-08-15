393dd86420cd0d46a945f68203aa1dde93ceebef
Douglas Fletcher <fletchy_d@hotmail.com>
2018-08-15 17:42:32 +0200

    DFO: master: move static to properties file

--- src/main/java/utility/InstanceSparkSession.java
+++ src/main/java/utility/InstanceSparkSession.java
@@ -1,5 +1,6 @@
 package utility;
 
+import base.config.SJConfig;
 import org.apache.spark.sql.SparkSession;
 
 public class InstanceSparkSession {
@@ -7,8 +8,8 @@ public class InstanceSparkSession {
     // singleton access for InstanceSparkContext
     private static SparkSession instance = SparkSession
             .builder()
-            .master("local[*]")
-            .appName("creditscoring")
+            .master(SJConfig.sparkSessionPropMaster.getValue())
+            .appName(SJConfig.sparkSessionPropAppname.getValue())
             .getOrCreate();
 
     private InstanceSparkSession(){};
