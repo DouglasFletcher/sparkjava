5920b1f64124fc0e48ea87170d6d69c9a802017a
Douglas Fletcher <-->
2018-08-15 17:02:21 +0200

    DFO: add tests

--- src/test/java/data/PrepRawdataTest.java
+++ src/test/java/data/PrepRawdataTest.java
@@ -3,6 +3,8 @@ package data;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.mllib.regression.LabeledPoint;
 import org.apache.spark.sql.SparkSession;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import utility.InstanceSparkSession;
 import utility.ProjectStaticVars;
@@ -15,6 +17,10 @@ import static org.junit.Assert.assertTrue;
 
 public class PrepRawdataTest {
 
+    private ReadDataset dataset;
+
+    private SparkSession spark;
+
     @Inject
     PrepRawdata prepRawdata;
 
@@ -24,18 +30,24 @@ public class PrepRawdataTest {
     @Inject
     InstanceSparkSession instanceSparkSession;
 
+    @Before
+    public void setUp() throws Exception {
+        // test data
+        spark = instanceSparkSession.getInstance();
+        String fileIn = projectStaticVars.createFileLoc("/01_data/cstraining_kaggle.csv");
+        dataset = new ReadDataset(spark, fileIn);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        //
+    }
+
     @Test
     public void createRandForestData_shouldSucceed(){//
-
         // expected
         int rowCount = 150000;
-
-        // test data
-        SparkSession spark = instanceSparkSession.getInstance();
-        String fileIn = projectStaticVars.createFileLoc("/01_data/cstraining_kaggle.csv");
-        ReadDataset dataset = new ReadDataset(spark, fileIn);
         dataset.readRawData();
-
         // tests method: rows check
         assertFalse(prepRawdata.createRandForestData(dataset.getPreppedData()).isEmpty());
         assertEquals(rowCount, dataset.getPreppedData().count());
--- src/test/java/data/ReadDatasetTest.java
+++ src/test/java/data/ReadDatasetTest.java
@@ -1,19 +1,49 @@
 package data;
 
+import org.apache.spark.sql.SparkSession;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import utility.InstanceSparkSession;
+import utility.ProjectStaticVars;
 
+import javax.inject.Inject;
+
+import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class ReadDatasetTest {
 
-    @Test
-    public void readRawData_shouldSucceed(){
-        assertTrue(true);
+    private ReadDataset dataset;
+
+    private SparkSession spark;
+
+    @Inject
+    ProjectStaticVars projectStaticVars;
+
+    @Inject
+    InstanceSparkSession instanceSparkSession;
+
+    @Before
+    public void setUp() throws Exception {
+        // test data
+        spark = instanceSparkSession.getInstance();
+        String fileIn = projectStaticVars.createFileLoc("/01_data/cstraining_kaggle.csv");
+        dataset = new ReadDataset(spark, fileIn);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        //
     }
 
     @Test
-    public void getPreppedData_shouldSucceed(){
-        assertTrue(true);
+    public void readRawData_shouldSucceed(){//
+        // expected
+        int rowCount = 150000;
+        // test method
+        dataset.readRawData();
+        assertEquals(rowCount, dataset.getPreppedData().count());
     }
 
 }
