387f351b0c5861985321050953b9706572a02f0f
douglas fletcher <fletchy_d@hotmail.com>
2018-08-15 09:35:46 +0200

    DFO: update tests

--- src/test/java/utility/ProjectStaticVarsTest.java
+++ src/test/java/utility/ProjectStaticVarsTest.java
@@ -2,18 +2,33 @@ package utility;
 
 import org.junit.Test;
 
+import javax.inject.Inject;
+
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
 
 public class ProjectStaticVarsTest {
 
+    private String projLoc = "C:/Users/dofletcher/Documents/deloitte/projects/creditscoring/";
+
+    private String testFile = "test.csv";
+
+    @Inject
+    ProjectStaticVars projectStaticVars;
+
     @Test
     public void createFileLoc_shouldSucceed(){
-        assertTrue(true);
+        String actual = projectStaticVars.createFileLoc("test.csv");
+        String expected = projLoc + testFile;
+        // run tests
+        assertEquals(expected, actual);
     }
 
     @Test
     public void getProjloc_shouldSucceed(){
-        assertTrue(true);
+
+        assertEquals(projLoc, projectStaticVars.getProjloc());
     }
 
 }
