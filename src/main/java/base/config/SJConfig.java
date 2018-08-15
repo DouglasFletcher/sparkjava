a6fedbc5d45f32725900caad71e5cd9d756ebec2
Douglas Fletcher <-->
2018-08-15 16:04:29 +0200

    DFO: project configuration

---
+++ src/main/java/base/config/SJConfig.java
@@ -0,0 +1,51 @@
+package base.config;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public enum SJConfig {
+
+    projectLocConfig("proj.config.location"); // NOSONAR
+
+    private final String propertyKey;
+
+    private String value;
+
+    static {
+        Properties prop = new Properties();
+        try (//
+            InputStream input = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream("config.properties");) {//
+                prop.load(input);//
+        } catch (IOException | IllegalArgumentException | NullPointerException exception) {
+            throw new RuntimeException("Could not load property file.", exception);// NOSONAR
+        }
+
+        for (SJConfig message : SJConfig.values()) {//
+            String value = prop.getProperty(message.propertyKey);//
+            if (value == null) {//
+                throw new RuntimeException("Property <" + message.propertyKey + "> not found in properties file!"); // NOSONAR
+            }
+            message.value = value;
+        }
+    }
+
+    private SJConfig(String propertyKey) {
+        this.propertyKey = propertyKey;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String format(Object... arguments) {
+        return String.format(value, arguments);
+    }
+}
+
