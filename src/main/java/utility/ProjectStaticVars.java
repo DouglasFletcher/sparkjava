ce364699f04706d04541458eb1bade7affaa6a15
Douglas Fletcher <-->
2018-08-17 11:00:29 +0200

    DFO: update java docs

--- src/main/java/utility/ProjectStaticVars.java
+++ src/main/java/utility/ProjectStaticVars.java
@@ -2,16 +2,31 @@ package utility;
 
 import base.config.SJConfig;
 
+/**
+ * <h3>Project Static variables</h3>
+ * <p>class to store project location variables.</p>
+ */
 public class ProjectStaticVars {
 
     public static final String projLoc = SJConfig.projectLocDirConfig.getValue();
 
+    /**
+     * <h3>File location</h3>
+     * <p>Instance for storing file location parameters</p>
+     * @param fileIn String: file location
+     * @return outfile: file location
+     */
     public static String createFileLoc(String fileIn){
         // file location
         String outfile = new StringBuilder(projLoc).append(fileIn).toString();
         return outfile ;
     }
 
+    /**
+     * <h3>get project location</h3>
+     * <p>get project location</p>
+     * @return projLoc String: get project location
+     */
     public static String getProjloc() {
         return projLoc;
     }
